886d40eeb2620466ea3e7132ca449eafdf65dd37
Toshio Watanabe <toshio_ws@hotmail.com>
2021-04-13 03:19:28 -0300

    Add: CSS and created Readme.md

---
+++ README.md
@@ -0,0 +1,33 @@
+# pomodoro-timer
+
+<h1 align="center">
+  <img alt="logo" src="" width="200"/>
+  <br>
+</h1>
+
+<p align="center">
+  <a href="#thinking-about">Sobre</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#user-content--preview">Preview</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#rocket-technologies">Tecnologia</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#information-source-how-to-use">Como rodar</a>
+</p>
+
+## About :thinking:
+
+<div>
+Projeto crud feito para o processo seletivo da Hepta.
+</div>
+
+
+## :eyeglasses: Preview
+![]()
+
+## :hammer:	Technologies
+This project was developed with these technologies:
+- [Vue](https://vuejs.org/)
+- [Html5]()
+- [Bootstrap](https://getbootstrap.com/)
+- [Java 8] ()
+
+## :question: How to use
+Clone this repo into the directory you want to use for your new project, install dependencies and build project:
--- funcionarios-front-end/src/App.vue
+++ funcionarios-front-end/src/App.vue
@@ -2,26 +2,32 @@
   <div id="app">
 
     <nav>
-      <div class="nav-wrapper green darken-1">
-      <a href="#!" class="brand-logo"><i class="material-icons">badge</i></a>
+      <div class="nav-wrapper orange darken-1">
+        <a href="#!" class="brand-logo"><img src="./assets/logo_01.png"></a>
         <a href="#" class="brand-logo center">Sistema de cadastro de funcionarios</a>
       </div>
     </nav>
 
     <div class="container">
 
+      <div class="alert alert-warning alert-dismissible fade show" role="alert" id="msg">
+        <div class="materialert success">
+          {{ mensagem }}
+        </div>
+      </div>
+
       <form @submit.prevent="salvar">
 
           <label>Nome</label>
-          <input type="text" placeholder="Nome" v-model="funcionario.nome">
+          <input type="text" placeholder="Nome" v-model="funcionario.nome" required>
           <label>Setor</label>
-          <input type="text" placeholder="Setor" v-model="funcionario.setor.nome">
+          <input type="text" placeholder="Setor" v-model="funcionario.setor.nome" required>
           <label>Salário</label>
-          <input type="number" placeholder="Salario" v-model="funcionario.salario">
+          <input type="number" placeholder="Salario" v-model="funcionario.salario" required>
           <label>Email</label>
-          <input type="text" placeholder="Email" v-model="funcionario.email">
+          <input type="text" placeholder="Email" v-model="funcionario.email" required>
           <label>Idade</label>
-          <input type="number" placeholder="Idade" v-model="funcionario.idade">
+          <input type="number" placeholder="Idade" v-model="funcionario.idade" required>
 
           <button class="waves-effect waves-light btn-small">Salvar<i class="material-icons left">save</i></button>
 
@@ -51,8 +57,8 @@
             <td>{{ funcionario.email }}</td> 
             <td>{{ funcionario.idade }}</td>
             <td>
-              <button @click="editar(funcionario)" class="waves-effect btn-small blue darken-1"><i class="material-icons">create</i></button>
-              <button @click="remover(funcionario.id)" class="waves-effect btn-small red darken-1"><i class="material-icons">delete_sweep</i></button>
+              <button @click="editar(funcionario)" class="waves-effect btn-small waves-light darken-1"><i class="material-icons">edit</i></button>
+              <button @click="remover(funcionario.id)" class="waves-effect btn-small waves-light darken-1"><i class="material-icons">delete_forever</i></button>
             </td>
 
           </tr>
@@ -83,7 +89,8 @@ export default {
         email: null,
         idade: null
       },
-      funcionarios:[]
+      funcionarios:[],
+      mensagem: null
     }
   },
 
@@ -107,6 +114,8 @@ export default {
 
       this.limpaCampo()
       this.listar()
+
+      this.msgBalao('Funcionario adicionado com sucesso!')
     }
     else {
       this.atualizar()
@@ -120,6 +129,7 @@ export default {
 
         this.limpaCampo()
         this.listar()
+        this.msgBalao('Funcionario atualizado com sucesso!')
       },
 
       editar(funcionario) {
@@ -127,21 +137,32 @@ export default {
       },
 
       async remover(id) {
-        console.log(id)
- 
-        await Funcionario.deletar(id)
-        .then()
-        .catch((err) => console.log(err))
-        
-        this.listar()
+        if(confirm('Deseja realmente remover o funcionário?')) {
+          await Funcionario.deletar(id)
+          .then()
+          .catch((err) => console.log(err))
+
+          this.listar()
+          
+          this.msgBalao('Funcionario excluido com sucesso!')
+        }
+
       },
 
       limpaCampo() {
+        this.funcionario.id = null
         this.funcionario.nome = null
         this.funcionario.setor.nome = null
         this.funcionario.salario = null
         this.funcionario.email = null
         this.funcionario.idade = null
+      },
+
+      msgBalao(mensagem) {
+        this.mensagem = mensagem
+        var s = document.getElementById('msg').style;
+        s.opacity = 3;
+        (function fade(){(s.opacity-=.1)<0?s.display="":setTimeout(fade,100)})();
       }
 
     }
@@ -150,5 +171,40 @@ export default {
 </script>
 
 <style>
+img {
+  width: 60px;
+  height: 60px;
+  margin-left: 10px;
+  margin-top: 2px;
+}
 
+#msg {
+  opacity: 0;
+}
+
+.materialert{
+    position: relative;
+    min-width: 150px;
+    padding: 15px;
+    margin-bottom: 20px;
+    margin-top: 15px;
+    border: 1px solid transparent;
+    border-radius: 4px;
+    transition: all 0.1s linear;
+    webkit-box-shadow: 0 2px 2px 0 rgba(0,0,0,0.14), 0 3px 1px -2px rgba(0,0,0,0.12), 0 1px 5px 0 rgba(0,0,0,0.2);
+    box-shadow: 0 2px 2px 0 rgba(0,0,0,0.14), 0 3px 1px -2px rgba(0,0,0,0.12), 0 1px 5px 0 rgba(0,0,0,0.2);
+    display: -webkit-box;
+    display: -webkit-flex;
+    display: -ms-flexbox;
+    display: flex;
+    -webkit-box-align: center;
+    -webkit-align-items: center;
+    -ms-flex-align: center;
+    align-items: center;
+}
+
+.materialert.success{
+    background-color: #43a047;
+    color: #fff;
+}
 </style>
